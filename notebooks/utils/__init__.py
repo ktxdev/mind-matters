8b50a2d28c16ed64107caf9048d8325ad0adf9d0
Sean Huvaya <sean.ktxdev@gmail.com>
2024-11-16 22:39:23 -0500

    Handle missing value for student based variables

--- notebooks/utils/__init__.py
+++ notebooks/utils/__init__.py
@@ -1,2 +1,2 @@
 from .plot import plot_numerical, plot_categorical
-from .utils import check_missing_values
+from .utils import check_missing_values, handle_group_specific_missing_values
--- notebooks/utils/utils.py
+++ notebooks/utils/utils.py
@@ -6,3 +6,20 @@ def check_missing_values(data: DataFrame, column: str):
     missing_values_count = missing_values_rows.sum()
     missing_values_percentage = round(missing_values_rows.mean() * 100, 2)
     print(f"Missing values count: {missing_values_count}\nMissing values percentage: {missing_values_percentage}%")
+
+
+def handle_group_specific_missing_values(data: DataFrame, column: str, group_column: str, group_value: str):
+    # Calculate median for the specific group
+    group_median = data.loc[data[group_column] == group_value, column].median()
+
+    # Fill missing values for the specific group
+    data.loc[data[group_column] == group_value, column] = data.loc[data[group_column] == group_value, column].fillna(
+        group_median)
+
+    # Fill remaining missing values with -1 for "Not Applicable"
+    data[column] = data[column].fillna(-1)
+
+    # Add an applicability column
+    data[f'{column} Applicable'] = data[group_column].apply(lambda x: 1 if x == group_value else 0)
+
+    return data
