08016ee74252cfe84131630ef3acdb41937123f9
Sean Huvaya <sean.ktxdev@gmail.com>
2024-11-18 14:01:54 -0500

    Handle missing value profession and financial stress

--- notebooks/utils/utils.py
+++ notebooks/utils/utils.py
@@ -1,4 +1,5 @@
 from pandas import DataFrame
+from typing import Dict
 
 
 def check_missing_values(data: DataFrame, column: str):
@@ -9,6 +10,7 @@ def check_missing_values(data: DataFrame, column: str):
 
 
 def handle_group_specific_missing_values(data: DataFrame, column: str, group_column: str, group_value: str):
+    data = data.copy()
     # Calculate median for the specific group
     group_median = data.loc[data[group_column] == group_value, column].median()
 
@@ -22,4 +24,33 @@ def handle_group_specific_missing_values(data: DataFrame, column: str, group_col
     # Add an applicability column
     data[f'{column} Applicable'] = data[group_column].apply(lambda x: 1 if x == group_value else 0)
 
+    return data
+
+
+def handle_profession_missing_values(data: DataFrame, values: Dict[str, str]):
+    """Fills missing values in the 'Profession' column based on the value in the
+    'Working Professional or Student' column.
+
+    Parameters:
+        data (pd.DataFrame): The input DataFrame
+        values (Dict[str, str]): Dictionary of values to fill in the missing values
+
+    Returns:
+        pd.DataFrame: The DataFrame with missing values in 'Profession' filled appropriately.
+    """
+    data = data.copy()
+
+    for key, value in values.items():
+        data.loc[
+            data['Working Professional or Student'] == key, 'Profession'
+        ] = data.loc[
+            data['Working Professional or Student'] == key, 'Profession'
+        ].fillna(value)
+
+    return data
+
+
+def handle_financial_stress_missing_values(data: DataFrame):
+    data = data.copy()
+    data['Financial Stress'] = data['Financial Stress'].fillna(data['Financial Stress'].median())
     return data
